3b3b3177d9486018a05c4dc2cae3f8411f6a430b
Vinicius <vinicius.devjvm@gmail.com>
2024-03-17 17:46:05 -0300

    feat: Construida a classe de inicializacao do servico

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,1 +1,40 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
 
+export class App {
+  private app: Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+    this.routes();
+    this.handleParserErros();
+  }
+
+  private config(): void {
+    this.app.use(
+      cors({
+        origin: '*',
+      }),
+    );
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  };
+
+  private routes(): void {
+  }
+
+  private handleParserErros(): void {
+    this.app.use(
+      errors({
+        statusCode: 422,
+      }),
+    );
+  }
+
+  public listen(port: Number): void {
+    this.app.listen(port || 3333);
+  }
+}
--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,4 +1,7 @@
+import { App } from './http/app';
+
 const PORT = Number(process.env.PORT) || 3333;
 
+const app = new App();
 
-console.log('Aqua');
+app.listen(PORT);
