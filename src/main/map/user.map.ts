28b50dc304b2e92654e44c974e8acfc0fe25b805
Vinicius <vinicius.devjvm@gmail.com>
2024-03-31 22:00:14 -0300

    cria a implementacao dos metodos Maps.

--- src/main/map/user.map.ts
+++ src/main/map/user.map.ts
@@ -1,13 +1,22 @@
 import { UserMapType } from './type/userMap.type';
 import { UserEntity, UserResponseType } from '../../domain/entity/user.entity';
 import { UserModel } from '../../application/user/typeorm/user.model';
+import { UserFactory } from '../../domain/factory/user.factory';
 
 export const UserMap: UserMapType = {
   DomainToResponse(entity: UserEntity): UserResponseType {
-    throw new Error('Method not implemented');
+    return {
+      id: entity.id,
+      name: entity.name,
+      email: entity.email,
+      password: entity.password,
+      resetPassword: entity.resetPassword,
+      created_at: entity.created_at,
+      updated_at: entity.updated_at,
+    };
   }, TypeormArrayToDomain(models: UserModel[]): UserEntity[] {
     throw new Error('Method not implemented');
   }, TypeormToDomain(model: UserModel): UserEntity {
-    throw new Error('Method not implemented');
+    return UserFactory.create({ ...model });
   },
 };
