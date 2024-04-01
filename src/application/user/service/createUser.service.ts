a38375b446d4364d5de12d7eab3dc607e61108b4
Vinicius <vinicius.devjvm@gmail.com>
2024-03-31 22:02:04 -0300

    cria a service de criacao do usuario.

--- src/application/user/service/createUser.service.ts
+++ src/application/user/service/createUser.service.ts
@@ -4,20 +4,31 @@ import { UserMap } from '../../../main/map/user.map';
 import { userRegisterRequestType } from '../controller/createUser.controller';
 import { UserFactory } from '../../../domain/factory/user.factory';
 import { generateUniqueId } from '../../../infra/utils/generateUniqueId';
+import { AlreadyExist } from '../../../lib/errros';
 
 export class CreateUserService {
-  constructor(private _userRepositoryInterface: UserRepositoryInterface) {
+  constructor(private _userRepository: UserRepositoryInterface) {
   }
 
   public async execute(data: userRegisterRequestType): Promise<UserResponseType> {
-    const userFactoryCreate = UserFactory.create({
+    const getUserRepositoryResponse = await this._userRepository.getUser(data.email);
+
+    if (getUserRepositoryResponse) {
+      throw new AlreadyExist('User already exists',
+        'usuario ja existente');
+    }
+
+    const userCreate = UserFactory.create({
       id: generateUniqueId(),
       name: data.name,
       email: data.email,
       password: data.password,
+      resetPassword: false,
     });
 
-    const userRepositoryResponse = await this._userRepositoryInterface.create(userFactoryCreate);
+    userCreate.hashPassword();
+
+    const userRepositoryResponse = await this._userRepository.create(userCreate);
 
     return UserMap.DomainToResponse(userRepositoryResponse);
   }
