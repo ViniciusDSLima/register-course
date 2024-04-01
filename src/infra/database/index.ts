041e72e2f411ecc92b0636993621bf38119d064e
Vinicius <vinicius.devjvm@gmail.com>
2024-03-31 21:58:09 -0300

    atualiza a porta do postgres.

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,14 +1,16 @@
 import { DataSource } from 'typeorm';
 import { UserModel } from '../../application/user/typeorm/user.model';
 import { CreateTableUsers1710716579865 } from './migrations/1710716579865-CreateTableUsers';
+import { AddColumnResetPasswordIndUsers1711899301866 } from './migrations/1711899301866-AddColumnResetPasswordIndUsers';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
-  port: 3333,
+  port: 5432,
   username: 'postgres',
   password: 'postgres',
   database: 'registerDB',
   entities: [UserModel],
-  migrations: [CreateTableUsers1710716579865],
+  migrations: [CreateTableUsers1710716579865,
+    AddColumnResetPasswordIndUsers1711899301866],
 });
