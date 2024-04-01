6691782a78869f754224579ce1af54e1ce0e1dff
Vinicius <vinicius.devjvm@gmail.com>
2024-03-31 21:54:01 -0300

    exportado a Rota para o app.

--- src/infra/http/api/routes/index.ts
+++ src/infra/http/api/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { UsersRoutes } from '../../../../application/user/routes/users.routes';
 
-const routes = Router();
+export const routes = Router();
 
 routes.use('/users', UsersRoutes);
 
